d81676b2c175a03fdf01e4793c6c81fd9972e9b0
nabilersyad <nabile95@hotmail.com>
2026-01-03 17:04:51 +0800

    data update bmf

--- bmf_geojson_converter.py
+++ bmf_geojson_converter.py
@@ -19,9 +19,20 @@ def extract_from_html(html_file):
         #with open(f"{base}_route.geojson", "w", encoding="utf-8") as f:
         #    json.dump(json.loads(geojson_match.group(1)), f, indent=2)
         routes = json.loads(geojson_match.group(1))
-        with open(f"{semantic_data_directory}{routes['name']}_route.geojson", "w", encoding="utf-8") as f:
+        route_name = routes["name"]
+
+        # Ensure properties exist
+        if "properties" not in routes or routes["properties"] is None:
+            routes["properties"] = {}
+
+        # Add / overwrite route metadata
+        routes["properties"]["route_id"] = route_name
+        routes["properties"]["route_code"] = route_name
+
+
+        with open(f"{semantic_data_directory}{route_name}_route.geojson", "w", encoding="utf-8") as f:
             json.dump(routes, f, indent=2)
-        print(f"Created {routes['name']}_route.geojson")
+        print(f"Created {route_name}_route.geojson")
 
     if pois_match:
         pois = json.loads(pois_match.group(1))
@@ -42,9 +53,9 @@ def extract_from_html(html_file):
         #    json.dump({"type": "FeatureCollection", "features": features},f,indent=2)
         pois_geojson = {"type": "FeatureCollection", "features": features} 
 
-        with open(f"{semantic_data_directory}{routes['name']}_pois.geojson", "w", encoding="utf-8") as f:
+        with open(f"{semantic_data_directory}{route_name}_pois.geojson", "w", encoding="utf-8") as f:
             json.dump(pois_geojson, f, indent=2)
-        print(f"Created {routes['name']}_pois.geojson")
+        print(f"Created {route_name}_pois.geojson")
 
     return routes, pois_geojson
 
